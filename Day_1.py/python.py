654fab873e21aad7c1b42fbd43511a491b0817e4
vivek chenna <vivekchenna2157@gmail.com>
2025-06-13 20:41:53 -0400

    listandtuple

--- Day_1.py/python.py
+++ Day_1.py/python.py
@@ -1,1 +1,9 @@
-print("vivek")
+# this is how we should print a name (anything) in python language 
+print("hello vivek")
+
+# we can print a value by assigning a value in the name 
+name = "dhrulaxi"
+print(name)
+
+
+
