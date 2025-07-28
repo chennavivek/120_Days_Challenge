85cf0711eecd15f2f25129ebfdff030aded29370
chennavivek <vivekchenna2157@gmail.com>
2025-07-28 17:36:49 -0400

    Delete Day_1.py directory

--- Day_1.py/python.py
+++
@@ -1,9 +0,0 @@
-# this is how we should print a name (anything) in python language 
-print("hello vivek")
-
-# we can print a value by assigning a value in the name 
-name = "dhrulaxi"
-print(name)
-
-
-
